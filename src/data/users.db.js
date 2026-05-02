6baabd8a459b5960e41a81b9de5b53f04382ba1e
EternalHunterGMZ <adonis.ae45@gmail.com>
2026-05-02 20:06:49 +0200

    refactor(db): move users data into users.db.js file

--- src/data/users.db.js
+++ src/data/users.db.js
@@ -1,6 +1,8 @@
 const dataBaseUsuarios = [
-{ id: 1, username: "pepe", email: "pepe@gmail.com", password: "1587" },
-{ id: 2, username: "neo", email: "neo@gmail.com", password: "123" }
+{ id: 1, username: "user1", email: "user1@gmail.com", password: "$2b$10$lfEf9aeep5fUkybs0Lyg0OCcJDoad/VTogbGK7ZF4fZcX.EUVP.u2", role: "moderator", active: true , loginAttempts: 0,blockedUntil: null}, // 1234
+{ id: 2, username: "user2", email: "user2@gmail.com", password: "$2b$10$T0F6eR9JoekNofa0u3olMuxDXWVkCC2zRbHXjloPvKOmqSzRHt4Zq", role: "user", active: false , loginAttempts: 0,blockedUntil: null}, // abcd
+{ id: 3, username: "user3", email: "user3@gmail.com", password: "$2b$10$7yMXPbJGuxSX4OyZoJPFTehWWad2e4XfwnlD8Wg00SrDv3sK/XsZW", role: "user", active: true, loginAttempts: 0 ,blockedUntil: null}, // pass123
+{ id: 4, username: "admin", email: "admin@gmail.com", password: "$2b$10$YBwQfrNjgB7K2CFy6akYL.uoPkt1lo3XRfkBJx1nukNsBGpNuo9La", role: "admin", active: true , loginAttempts: 0,blockedUntil: null} // admin
 ];
 
 module.exports = dataBaseUsuarios;
