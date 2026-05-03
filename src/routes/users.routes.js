dbbd3c093e5358217d2dc2331e5b9a515fbc127f
EternalHunterGMZ <adonis.ae45@gmail.com>
2026-05-03 18:45:03 +0200

    refactor(routes): update routes with authentication and authorization middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middlewares/verifyToken.middleware");
+const checkPermission = require("../middlewares/permission.middleware");
+const checkOwnershipOrRole = require("../middlewares/ownership.middleware");
 
 const {
     getUsersController,
@@ -7,19 +10,18 @@ const {
     getUserByIdController,
     updateUserController,
     deleteUserController,
-    getUsersController
 } = require("../controllers/users.controller");
 
 // Obtener todos los usuarios
-router.get("/users", getUsersController);
-// Crear un usuario nuevi
-router.post("/users", createUserController);
+router.get("/users",verifyToken,checkPermission("read_users"),getUsersController);
+// Crear un usuario nuevo
+router.post("/users",verifyToken,checkPermission("create_user"), createUserController);
 // Obtener usuario por ID
-router.get("/users/:id", getUserByIdController);
+router.get("/users/:id", verifyToken,checkOwnershipOrRole("admin"), getUserByIdController);
 // Actualizar usuario
-router.put("/users/:id", updateUserController);
+router.put("/users/:id",verifyToken, checkOwnershipOrRole("admin"), updateUserController);
 // Eliminar usuario
-router.delete("/users/:id", deleteUserController);
+router.delete("/users/:id",verifyToken,checkPermission("delete_user"), deleteUserController);
 
 
 module.exports = router;
