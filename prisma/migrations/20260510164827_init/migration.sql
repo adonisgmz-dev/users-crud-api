21b4174c143e1eeb0c34c07c5bd77bc99b8b6fa3
EternalHunterGMZ <adonis.ae45@gmail.com>
2026-05-10 19:35:08 +0200

    feat(database): setup Prisma ORM with PostgreSQL

---
+++ prisma/migrations/20260510164827_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "secondLastName" TEXT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "loginAttempts" INTEGER NOT NULL DEFAULT 0,
+    "blockedUntil" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
