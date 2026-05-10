72522cb0dc67d33a5eab1ebcf7dfc54c6168dd76
EternalHunterGMZ <adonis.ae45@gmail.com>
2026-05-10 17:34:22 +0200

    feat(security): add helmet cors and rate limiting middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,42 @@
 const express = require("express");
 require("dotenv").config();
-const cookieParser= require("cookie-parser")
+const cookieParser = require("cookie-parser");
+// Seguridad
+
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
 // Importar rutas
 const usersRoutes = require("./routes/users.routes");
 const authRoutes = require("./routes/auth.routes");
+
+// Importar Middleware global de errores
 const errorMiddleware = require("./middlewares/error.middleware");
 
 const app = express();
-app.use(express.json());
+
+// Middlewares globales
+app.use(helmet());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Demasiadas peticiones. Intenta más tarde."
+  }
+});
+app.use(limiter);
+
 app.use(cookieParser());
+app.use(express.json());
+
+// Rutas
 app.use(usersRoutes);
 app.use(authRoutes);
 // Errores globales
